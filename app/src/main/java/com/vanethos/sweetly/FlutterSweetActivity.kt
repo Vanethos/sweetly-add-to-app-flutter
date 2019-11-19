ed2c63fa79c89112ab3c5836d454d49c52dd2630
Gonçalo Palma <solid.goncalo@gmail.com>
2019-11-19 17:08:18 +0400

    Change to FlutterSweetActivity
    - Instead of using FlutterActivity, we are going to directly extend it so that we can make further adjustments to it

---
+++ app/src/main/java/com/vanethos/sweetly/FlutterSweetActivity.kt
@@ -0,0 +1,13 @@
+package com.vanethos.sweetly
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+
+/**
+ * Created by goncalopalma on 2019-11-19
+ * TODO: Class header comment
+ */
+class FlutterSweetActivity : FlutterActivity() {
+    
+}
