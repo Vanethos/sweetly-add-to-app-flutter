3a824db067f319a54adf1331b90f13edb6b2325e
Gonçalo Palma <solid.goncalo@gmail.com>
2019-11-19 15:36:59 +0400

    Added list of items to the flutter app and exit functionality

--- sweetly_flutter/lib/main.dart
+++ sweetly_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,39 +26,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      body: ListView(
+          children: sweets.map((item) => SweetlyListItem(item: item, clickListener: navigateToNative,)).toList()),
     );
   }
+
+  navigateToNative(Sweet item) {
+    SystemNavigator.pop();
+  }
+}
+
+class SweetlyListItem extends StatelessWidget {
+  final Sweet item;
+  final SweetClick clickListener;
+
+  SweetlyListItem({this.item, this.clickListener, key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () => clickListener(item),
+        child: Card(
+            child: Row(children: [
+          Expanded(
+            flex: 2,
+            child: SizedBox(height: 75, child: Image.network(item.urlImage)),
+          ),
+          Expanded(
+            flex: 5,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  item.name,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.start,
+                ),
+                Text(item.country.toUpperCase(), textAlign: TextAlign.start),
+              ],
+            ),
+          )
+        ])));
+  }
+}
+
+typedef SweetClick(Sweet item);
+
+final List<Sweet> sweets = [
+  Sweet("Pastel de Nata", "Portugal",
+      "https://www.pingodoce.pt/wp-content/uploads/2016/10/pasteldenata.jpg"),
+  Sweet("Esse de Azeitão", "Portugal",
+      "http://www.docesregionais.com/wp-content/uploads/2013/06/Esses-de-Azeit%C3%A3o.jpg"),
+  Sweet("Dom Rodrigo", "Portugal",
+      "https://www.amodadoflavio.pt/wp-content/uploads/2017/05/dom-rodrigo-ok-999x500.jpg"),
+];
+
+class Sweet {
+  String name;
+  String country;
+  String urlImage;
+
+  Sweet(this.name, this.country, this.urlImage);
 }
